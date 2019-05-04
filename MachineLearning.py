2ce699ff26ceea95c7de9e23252731af34246fae
Alexandros Antoniadis <alexandros.antoniadis@astro.up.pt>
2019-05-04 20:02:18 +0100

    most recent

--- MachineLearning.py
+++ MachineLearning.py
@@ -34,9 +34,16 @@ def ML(regression):
     elif regression == 'ridge':
         reg = linear_model.Ridge(alpha=1)
     elif regression == 'ridgecv': 
-        reg = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0],cv=10,gcv_mode=None)
-    elif regression == 'lassolars':
-        reg = linear_model.LassoLars(alpha=1)
+        reg = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0],cv=5,gcv_mode=None)
+    elif regression == 'multitasklasso':
+        reg = linear_model.MultiTaskLasso(alpha=1) 
+    elif regression == 'multitaskelasticnet':
+        reg = linear_model.MultiTaskElasticNet(alpha=1) 
+        
+#    elif regression == 'lasso':
+#        reg = linear_model.Lasso(alpha=1)       
+#    elif regression == 'lassolars':
+#        reg = linear_model.LassoLars(alpha=1)
         
      
     spectralist = np.loadtxt('final1Dfilelist.dat', dtype=str)
@@ -53,50 +60,43 @@ def ML(regression):
         os.makedirs(MLplots_folder)  
              
     for i in np.arange(len(filepaths)):
-        
-
-        
+                
         df = pd.read_csv('conv'+resolution[i]+'_goodEWPar.csv')
         df.dropna(axis=1, inplace=True)
         
         names = ['names']
         
-        y = df.columns.values[-2:]
-        
-        labels = df[names]
-        
-        
-        
-        
-        
+        y = df.columns.values[-2:]               
         
         newlines = np.loadtxt('./'+directory_name+filepaths[i].replace('.fits','').replace('spectra/'+'newstars/','')+'centrallines.dat',dtype=str)
         
         newdf = pd.read_csv('./'+directory_name+filepaths[i].replace('.fits','').replace('spectra/'+'newstars/','')+'_newstars.csv')
         
             
-        newdf.dropna(axis=1, inplace=True) 
+        newdf.dropna(axis=1, inplace=True)
         
+###### masking possible gaps #################
+        zeroews = newdf.mask(newdf<0.00001)
+        newnames = ['newstars']
+        newlabels = newdf[newnames]
+        newdf = zeroews.dropna('columns')
+        newlines = newdf.columns.values[1::]        
+###############################################
         
         df_x = df.drop(y,axis=1)
         
         df_y = df[y] 
-        
-        
-        newnames = ['newstars']
-        newlabels = newdf[newnames] 
-        
-           
-        
+                
+#        newnames = ['newstars']
+#        newlabels = newdf[newnames] 
+                           
         df_x = pd.concat([df.loc[:, 'names'], df.loc[:, newlines]], axis=1)
                 
-        newdf = newdf.drop(newlabels,axis=1) 
+#       newdf = newdf.drop(newlabels,axis=1) 
         
         newdf = newdf.loc[:, newlines]
         
-        
-        
-            
+                            
         x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.30)
         
         labels_train = x_train[names]
@@ -107,7 +107,6 @@ def ML(regression):
         
         reg.fit(x_train, y_train)
         
-        #for saving the model run the next line. For keeping current,comment it out
         joblib.dump(reg, 'savedmodel.pkl')
         
         y_pred_test = reg.predict(x_test)
@@ -128,9 +127,9 @@ def ML(regression):
         r2score = r2_score(y_test, y_pred_test)
         
         
-        reg = joblib.load('savedmodel.pkl') #for loading the saved model
+        reg = joblib.load('savedmodel.pkl') #loading the saved model
         
-        newdf = reg.predict(newdf) #applying the saved model to the new stars
+        newdf = reg.predict(newdf) #applying the saved model to the new star
         
         finalresults = pd.concat([newlabels, pd.DataFrame(newdf)], axis=1)
         print(finalresults)
@@ -151,15 +150,13 @@ def ML(regression):
         
         print('Mean Absolute Error of Test : ' + str(mae_test))
         #print('Mean Absolute Percentage Error of Test : '+ str(mape_test))
-        
-        
+                
         
         train_givenvalues = pd.concat([labels_train, y_train], axis=1)
         train_givenvalues = train_givenvalues.reset_index(drop=True)
         new_labeltrain=labels_train.reset_index(drop=True)
         train_predvalues = pd.concat([new_labeltrain, pd.DataFrame(y_pred_train)], axis=1)
-        
-        
+                
         test_givenvalues = pd.concat([labels_test, y_test], axis=1)
         test_givenvalues = test_givenvalues.reset_index(drop=True)
         new_labeltest=labels_test.reset_index(drop=True)
@@ -167,61 +164,71 @@ def ML(regression):
         
         res_file.write(finalresults.to_string(index=False,header=False)+' '+ str(mae_test[0])+' '+ str(mae_test[1])+' '+ str(r2score)+' '+ str(variancescore) + "\n")
         
-        # plotting the train FeH
+        
         starname = filepaths[i].replace('.fits','').replace('spectra/'+'newstars/','')
         
-        
+        # plotting the train FeH
         set_res = 15
         
         plt.figure(figsize=([set_res,set_res]))
-        plt.title(starname+' [Fe/H]'+' '+'train'+' '+'comparison', fontsize=set_res*1.2)
-        plt.ylabel("ML value", fontsize=set_res*1.5)
-        plt.xlabel("AA value", fontsize=set_res*1.5)
+        plt.title(starname+' [Fe/H]'+' '+'train'+' '+'comparison', fontsize=set_res*1.5)
+        plt.ylabel("ML [Fe/H] [dex]", fontsize=set_res*1.5)
+        plt.xlabel("AA [Fe/H] [dex]", fontsize=set_res*1.5)
         plt.tick_params(axis='both',labelsize=set_res*1.5)
-        plt.plot((-0.8,0.8),(-0.8,0.8),'--b') # for Fe/H
+        plt.plot((-0.8,0.4),(-0.8,0.4),'--b') # for Fe/H
         plt.plot(train_givenvalues.values[:,1], train_predvalues.values[:,1],'o')
-        #plt.show()    
-        plt.savefig("./"+MLplots_folder+"/"+starname+'_FeH_train_comparison.png', bbox_inches='tight')
+        #plt.show()
+        plt.tick_params(axis='both',labelsize=set_res*1.5)
+        plt.plot(clip_box=True, clip_on=True)
         plt.tight_layout()
+        plt.savefig("./"+MLplots_folder+"/"+starname+'_FeH_train_comparison.png', bbox_inches='tight')
+        
         # plotting the train Teff
         set_res = 15
         
         plt.figure(figsize=([set_res,set_res]))
-        plt.title(starname+' Teff'+' '+'train'+' '+'comparison', fontsize=set_res*1.2)
-        plt.ylabel("ML value", fontsize=set_res*1.5)
-        plt.xlabel("AA value", fontsize=set_res*1.5)
+        plt.title(starname+' T$_{\mathrm{eff}}$'+' '+'train'+' '+'comparison', fontsize=set_res*1.5)
+        plt.ylabel("ML T$_{\mathrm{eff}}$ [K]", fontsize=set_res*1.5)
+        plt.xlabel("AA T$_{\mathrm{eff}}$ [K]", fontsize=set_res*1.5)
         plt.tick_params(axis='both',labelsize=set_res*1.5)
-        plt.plot((2000,4000),(2000,4000),'--b') #for Teff    
+        plt.plot((2700,4000),(2700,4000),'--b') #for Teff    
         plt.plot(train_givenvalues.values[:,2], train_predvalues.values[:,2],'o')    
         #plt.show()
+        plt.tick_params(axis='both',labelsize=set_res*1.5)
+        plt.plot(clip_box=True, clip_on=True)
+        plt.tight_layout()        
         plt.savefig("./"+MLplots_folder+"/"+starname+'_Teff_train_comparison.png', bbox_inches='tight')
-        plt.tight_layout()
-            
+                   
         # plotting the test FeH
         set_res = 15
         
         plt.figure(figsize=([set_res,set_res]))
-        plt.title(starname+' [Fe/H]'+' '+'test'+' '+'comparison', fontsize=set_res*1.2)
-        plt.ylabel("ML value", fontsize=set_res*1.5)
-        plt.xlabel("AA value", fontsize=set_res*1.5)
+        plt.title(starname+' [Fe/H]'+' '+'test'+' '+'comparison', fontsize=set_res*1.5)
+        plt.ylabel("ML [Fe/H] [dex]", fontsize=set_res*1.5)
+        plt.xlabel("AA [Fe/H] [dex]", fontsize=set_res*1.5)
         plt.tick_params(axis='both',labelsize=set_res*1.5)
-        plt.plot((-0.8,0.8),(-0.8,0.8),'--b') # for FeH
+        plt.plot((-0.8,0.4),(-0.8,0.4),'--b') # for FeH
         plt.plot(test_givenvalues.values[:,1], test_predvalues.values[:,1],'o')
         #plt.show() 
+        plt.tick_params(axis='both',labelsize=set_res*1.5)
+        plt.plot(clip_box=True, clip_on=True)
+        plt.tight_layout()        
         plt.savefig("./"+MLplots_folder+"/"+starname+'_FeH_test_comparison.png', bbox_inches='tight')
-        plt.tight_layout()
         
         # plotting the test Teff
         set_res = 15
         
         plt.figure(figsize=([set_res,set_res]))
-        plt.title(starname+' Teff'+' '+'test'+' '+'comparison', fontsize=set_res*1.2)
-        plt.ylabel("ML value", fontsize=set_res*1.5)
-        plt.xlabel("AA value", fontsize=set_res*1.5)
+        plt.title(starname+' T$_{\mathrm{eff}}$'+' '+'test'+' '+'comparison', fontsize=set_res*1.5)
+        plt.ylabel("ML T$_{\mathrm{eff}}$ [K]", fontsize=set_res*1.5)
+        plt.xlabel("AA T$_{\mathrm{eff}}$ [K]", fontsize=set_res*1.5)
         plt.tick_params(axis='both',labelsize=set_res*1.5)
-        plt.plot((2000,4000),(2000,4000),'--b') #for Teff
+        plt.plot((2700,4000),(2700,4000),'--b') #for Teff
         plt.plot(test_givenvalues.values[:,2], test_predvalues.values[:,2],'o')
         #plt.show()
+        plt.tick_params(axis='both',labelsize=set_res*1.5)
+        plt.plot(clip_box=True, clip_on=True)
+        plt.tight_layout()        
         plt.savefig("./"+MLplots_folder+"/"+starname+'_Teff_test_comparison.png', bbox_inches='tight')
-        plt.tight_layout()
+        
     res_file.close()        
