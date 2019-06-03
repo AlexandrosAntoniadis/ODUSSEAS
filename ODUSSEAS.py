e8c3701fbb69b60c8e9ecefcc80f6871e6ebcbf2
AlexandrosAntoniadis <32932882+AlexandrosAntoniadis@users.noreply.github.com>
2019-06-03 14:49:30 +0100

    Update ODUSSEAS.py

--- ODUSSEAS.py
+++ ODUSSEAS.py
@@ -14,18 +14,17 @@ import MachineLearning
 
 option = "measure" # choose "measure" to measure the EWs of the new stars and get parameters with ML, or "justML" to get results with different run/model 
 regression = 'ridge' # choose the ML model: linear, ridge, ridgecv, multitasklasso, multitaskelasticnet  (Recommended: ridge)
-convo_limit = 100000 # if the resolution of the new spectra is higher than this, they will be convolved to their own resolution too.
 
 ########################
 
 
 if option == "measure" :
-    New_dataset.EWmeasurements(convo_limit)     
+    New_dataset.EWmeasurements()     
     
     MachineLearning.ML(regression)
 
 elif option =="measure_withrv":
-    New_dataset_withrv.EWmeasurements(convo_limit)     
+    New_dataset_withrv.EWmeasurements()     
     
     MachineLearning.ML(regression)
 
