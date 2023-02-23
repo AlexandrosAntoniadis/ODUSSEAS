6d1b8996640d0965db5b8bacbdced32ef56743d3
AlexandrosAntoniadis <32932882+AlexandrosAntoniadis@users.noreply.github.com>
2023-02-23 16:52:17 +0000

    Update ODUSSEAS.py

--- ODUSSEAS.py
+++ ODUSSEAS.py
@@ -11,7 +11,7 @@ import MachineLearning
 
 
 ######## SETTINGS ##############
-reference = 'photometry' # choose the reference scale: 'photometry' for 65 stars with Teff from Casagrande08 and [Fe/H] from Neves12, or 'interferometry' for 48 stars with Teff from Khata21 and [Fe/H] from Neves14 
+reference = 'interferometry' # choose the reference scale: 'photometry' for 65 stars with Teff from Casagrande08 and [Fe/H] from Neves12, or 'interferometry' for 48 stars with Teff from Khata21 and [Fe/H] from Neves14 
 regression = 'ridge' # choose the ML model: linear, ridge, ridgecv, multitasklasso, multitaskelasticnet  (Recommended: ridge)
 do_rv_cor = 'yes' # choose 'yes' if you want to do rv correction to the spectra or 'no' if they are already corrected
 ########################
